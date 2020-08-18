1fdaf1410892bca8064b890b71a9f592c4f1b50d
wandl <hnxyhcwdl1003@163.com>
2020-08-18 23:44:09 +0800

    优化异常返回状态

--- src/main/java/org/springframework/security/boot/ldap/authentication/LdapAuthenticationFailureHandler.java
+++ src/main/java/org/springframework/security/boot/ldap/authentication/LdapAuthenticationFailureHandler.java
@@ -75,7 +75,7 @@ public class LdapAuthenticationFailureHandler extends ExceptionMappingAuthentica
 	protected void writeJSONString(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException e) throws IOException, ServletException{
 		
-		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		response.setStatus(HttpStatus.OK.value());
 		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 		
